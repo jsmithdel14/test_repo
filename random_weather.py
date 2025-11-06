07495818f502d8009183811b9b1c1f945a4b4950
jsmithdel14 <jsmithdel14@gmail.com>
2025-11-06 10:16:23 -0700

    create test for get_random_weather

--- random_weather.py
+++ random_weather.py
@@ -4,4 +4,4 @@ def get_random_weather():
     weather = ["Sunny", "Rainy", "Cloudy", "Stormy", "Snowy"]
     temperature = random.randint(-10, 40)
 
-    print(f"Today's weather: {random.choice(weather)}, {temperature}°C")
+    return f"Today's weather: {random.choice(weather)}, {temperature}°C"
---
+++ test_random_weather.py
@@ -0,0 +1,3 @@
+from random_weather import get_random_weather
+def test_get_random_weather():
+    assert get_random_weather() == str
